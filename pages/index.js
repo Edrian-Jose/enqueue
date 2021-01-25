99910b06a46413a07cdbcb43d3e4bbe74f953f96
Edrian Jose Ferrer <44691783+Edrian-Jose@users.noreply.github.com>
2021-01-25 22:40:03 +0800

    restructure the project

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Navbar from "../components/Navbar";
+import Navbar from "../app/components/modules/Navbar/Navbar";
 
 export default function Home() {
   return (

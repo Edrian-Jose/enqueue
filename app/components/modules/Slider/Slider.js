5aa5e6785e43ac2bbb61fa79218b721b2f7c1fbf
Edrian Jose Ferrer <44691783+Edrian-Jose@users.noreply.github.com>
2021-01-26 18:34:48 +0800

    Implement slideshow in slidebar.js

--- app/components/modules/Slider/Slider.js
+++ app/components/modules/Slider/Slider.js
@@ -1,15 +1,55 @@
+import React, { useState, useEffect } from "react";
 import styles from "./Slider.module.scss";
 
 export default function Slider() {
+  const slideImagesSrc = [
+    "/images/1.jpg",
+    "/images/2.jpg",
+    "/images/3.jpg",
+    "/images/4.jpg",
+  ];
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const slidesContainerStyle = {
+    marginLeft: "-" + currentSlide * 100 + "vw",
+  };
+
+  useEffect(() => {
+    let interval = null;
+
+    interval = setInterval(() => {
+      setCurrentSlide((currentSlide) =>
+        currentSlide == slideImagesSrc.length - 1 ? 0 : currentSlide + 1
+      );
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [currentSlide]);
+
+  const slideButtons = slideImagesSrc.map((value, i) => {
+    let classes = styles.sliderButton;
+
+    if (i == currentSlide) {
+      classes += " " + styles.active;
+    }
+
+    return (
+      <div
+        className={classes}
+        key={"slideButton" + i}
+            onClick={() => { setCurrentSlide(i) }}
+      ></div>
+    );
+  });
+
+  const slideImages = slideImagesSrc.map((value, i) => {
+    return <img src={value} alt="SlideImage" key={"slideImage" + i} />;
+  });
+
   return (
     <div className={styles.sliderContainer}>
       <div className={styles.sliderOverlay}>
-        <div className={styles.sliderButtonsContainer}>
-          <div className={styles.sliderButton}></div>
-          <div className={styles.sliderButton}></div>
-          <div className={styles.sliderButton}></div>
-          <div className={styles.sliderButton}></div>
-        </div>
+        <div className={styles.sliderButtonsContainer}>{slideButtons}</div>
         <div className={styles.sliderOverlayContent}>
           <div className={styles.appTagline}>
             Best Business services are waiting for you
@@ -21,11 +61,8 @@ export default function Slider() {
           <div className={styles.getStartedButton}>{"Get Started >"}</div>
         </div>
       </div>
-      <div className={styles.slidesContainer}>
-        <img src="/images/1.jpg" alt="hello" />
-        <img src="/images/2.jpg" alt="hello" />
-        <img src="/images/3.jpg" alt="hello" />
-        <img src="/images/4.jpg" alt="hello" />
+      <div className={styles.slidesContainer} style={slidesContainerStyle}>
+        {slideImages}
       </div>
     </div>
   );

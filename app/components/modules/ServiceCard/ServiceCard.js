d7e92a1706c21a3883f3c6e4511f0362a3280f28
Edrian Jose Ferrer <44691783+Edrian-Jose@users.noreply.github.com>
2021-02-22 22:18:02 +0800

    create serarch page

---
+++ app/components/modules/ServiceCard/ServiceCard.js
@@ -0,0 +1,23 @@
+import Title from "./../../elements/Title/Title";
+import StarRating from "./../../elements/StarRating/StarRating";
+import Subtitle from "./../../elements/Subtitle/Subtitle";
+
+export default function ServiceCard({ service, className }) {
+  return (
+    <div className={"p-12 border " + className}>
+      <div className="flex items-center my-3">
+        <Title>{service.name}</Title>
+      </div>
+      <div className="my-3">
+        <StarRating rating={service.rating} reviewCount={service.reviewCount} />
+        <Subtitle className="ml-4">{service.type}</Subtitle>
+      </div>
+      <div className="my-3">
+        <Subtitle>{service.time}</Subtitle>
+      </div>
+      <div className="my-3">
+        <Subtitle>{service.address}</Subtitle>
+      </div>
+    </div>
+  );
+}
--- pages/search.js
+++ pages/search.js
@@ -2,10 +2,65 @@ import Head from "next/head";
 import Navbar from "../app/components/modules/Navbar/Navbar";
 import Footer from "../app/components/modules/Footer/Footer";
 import { useRouter } from "next/router";
+import ServiceCard from "./../app/components/modules/ServiceCard/ServiceCard";
+import Link from "next/link";
 
 export default function search() {
   const router = useRouter();
   const { q } = router.query;
+  const services = [
+    {
+      id: "xAdq1",
+      name: "Remudaro Boongaling Dental Clinic",
+      rating: 3.7,
+      reviewCount: 59,
+      type: "Dental Clinic",
+      address: "1, J.P Rizal Avenue, Manggahan",
+      time: "8:00 AM -11:00 AM, 12:00PM - 5:00 PM",
+    },
+    {
+      id: "xAdq2",
+      name: "Blang Blang Dental Clinic",
+      rating: 4.7,
+      reviewCount: 5,
+      type: "Dental Clinic",
+      address: "1, J.P Rizal Avenue, Manggahan",
+      time: "8:00 AM -11:00 AM, 12:00PM - 5:00 PM",
+    },
+    {
+      id: "xAdq3",
+      name: "Super Dental Clinic",
+      rating: 4.7,
+      reviewCount: 57,
+      type: "Dental Clinic",
+      address: "1, J.P Rizal Avenue, Manggahan",
+      time: "8:00 AM -11:00 AM, 12:00PM - 5:00 PM",
+    },
+    {
+      id: "xAdq4",
+      name: "Ultra Dental Clinic",
+      rating: 1.5,
+      reviewCount: 160,
+      type: "Dental Clinic",
+      address: "1, J.P Rizal Avenue, Manggahan",
+      time: "8:00 AM -11:00 AM, 12:00PM - 5:00 PM",
+    },
+  ];
+
+  const sortedServices = services.sort((a, b) => {
+    return a.rating * a.reviewCount - b.rating * b.reviewCount;
+  });
+
+  const serviceCards = sortedServices.map((service) => {
+    return (
+      <Link href={"/service/" + service.id} key={service.id}>
+        <a>
+          <ServiceCard service={service} />
+        </a>
+      </Link>
+    );
+  });
+
   return (
     <div>
       <Head>
@@ -14,7 +69,8 @@ export default function search() {
       </Head>
 
       <Navbar />
-      <h1>Search {q}</h1>
+      <div className="px-20">{serviceCards}</div>
+
       <Footer />
     </div>
   );

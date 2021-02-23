e08ef703e69a33c295391df2c9eb05de2ac39a16
Edrian Jose Ferrer <44691783+Edrian-Jose@users.noreply.github.com>
2021-02-23 14:02:53 +0800

    install joi and jsonwebtoken

--- app/models/User.js
+++ app/models/User.js
@@ -3,12 +3,31 @@ import mongoose from "mongoose";
 /* PetSchema will correspond to a collection in your MongoDB database. */
 const UserSchema = new mongoose.Schema({
   name: {
-    /* The name of this pet */
-
+    first: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    last: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+  },
+  email: {
     type: String,
-    required: [true, "Please provide a name for this pet."],
-    maxlength: [20, "Name cannot be more than 60 characters"],
+    trim: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    minlength: 7,
+    required: true,
   },
 });
+
+UserSchema.methods.fullname = function () {
+  return `${this.name.first} ${this.name.last}`;
+};
 
 export default mongoose.models.User || mongoose.model("User", UserSchema);

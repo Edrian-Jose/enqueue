b50fd4fa22b6e5c312f2803ce72c5088f64d83a0
Edrian Jose Ferrer <44691783+Edrian-Jose@users.noreply.github.com>
2021-02-25 00:44:06 +0800

    fix response status code of login.ja

--- pages/api/login.js
+++ pages/api/login.js
@@ -36,7 +36,7 @@ export default async function handler(req, res) {
     }
 
     return res
-      .status(400)
+      .status(200)
       .json({ success: true, data: user.generateAuthToken() });
   }
 }

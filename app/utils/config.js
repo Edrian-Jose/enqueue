89b7a5026d5692dab44a0145d44aea925c23aeae
Edrian Jose Ferrer <44691783+Edrian-Jose@users.noreply.github.com>
2021-02-25 18:24:25 +0800

    create config.js

--- app/utils/config.js
+++ app/utils/config.js
@@ -1,6 +1,16 @@
+import path from "path";
+import getConfig from "next/config";
 const dev = process.env.NODE_ENV !== "production";
+
+export const serverPath = (staticFilePath) => {
+  return path.join(
+    getConfig().serverRuntimeConfig.PROJECT_ROOT,
+    staticFilePath
+  );
+};
 
 export const server = dev
   ? "http://localhost:3000"
   : "https://enqueue.vercel.app";
-export const publicFolder = dev ? "./public/" : "/";
+
+export const publicFolder = dev ? "./public/" : serverPath;

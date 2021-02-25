10f83293f086bc32962b4b0db02a70b1473c943e
Edrian Jose Ferrer <44691783+Edrian-Jose@users.noreply.github.com>
2021-02-25 18:27:45 +0800

    change config

--- app/utils/config.js
+++ app/utils/config.js
@@ -2,12 +2,7 @@ import path from "path";
 import getConfig from "next/config";
 const dev = process.env.NODE_ENV !== "production";
 
-export const serverPath = (staticFilePath) => {
-  return path.join(
-    getConfig().serverRuntimeConfig.PROJECT_ROOT,
-    staticFilePath
-  );
-};
+export const serverPath = getConfig().serverRuntimeConfig.PROJECT_ROOT;
 
 export const server = dev
   ? "http://localhost:3000"
